039b6448c5d87563491ce789bc6c6a19bad83881
Johnny Denford <jpdenford@gmail.com>
2017-02-26 17:30:39 +1300

    Update to all use async await

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function checkStatus() {
   const curStatus = readDoor();
   if(curStatus != prevStatus){
     prevStatus = curStatus;
-    logger.info('Door changed to '+ curStatus);
+    logger.info('Door changed to ' + curStatus);
     const msg = curStatus == DOOR_OPEN? OPEN_MESSAGE : CLOSED_MESSAGE;
     // update and save the timestamp of the message
     updateSlack(msg, prevMsgTimestamp).then(ts => {prevMsgTimestamp = ts;});
@@ -42,32 +42,23 @@ function readDoor(){
   return newStat;
 }
 
-function updateSlack(status, prevMsgTimestamp) {
-  return new Promise(function(fulfill, reject) {
-    const text = status;
-    // auxillary function to construct request params
-    // const res = await request('xx')
-    // if(res.bla){
-    //   dosomthing
-    // }
-
-    request(getMessagesOpts(1)).then(res => {
-      const lastSlackMessage = res.messages[0]
-      // TODO flatten this using Promise control flows
-      if(lastSlackMessage && prevMsgTimestamp && lastSlackMessage.ts == prevMsgTimestamp) {
-        // update message
-        request(updateMessageOpts(text, prevMsgTimestamp)).then(res => {
-          fulfill(res.ts); // return timstamp
-        }, reject);
-      } else {
-        // post new
-        request(newMessageOpts(text)).then(res => {
-          fulfill(res.ts) // return timestamp
-        }, reject);
-      }
-      console.log(res);
-    }, logger.error);
-  });
+async function updateSlack(status, prevMsgTimestamp) {
+  try{
+    const historyRes = await request(getMessagesOpts(1))
+    const lastSlackMessage = historyRes.messages[0]
+    // TODO flatten this using Promise control flows
+    if(lastSlackMessage && prevMsgTimestamp && lastSlackMessage.ts == prevMsgTimestamp) {
+      // update message
+      const updatedMessage = await request(updateMessageOpts(status, prevMsgTimestamp))
+      return updatedMessage.ts
+    } else {
+      // post new
+      const newMessageRes = await request(newMessageOpts(status))
+      return newMessageRes.ts
+    }
+  } catch(e){
+    logger.error('Slack Update Failed' + e);
+  }
 }
 
 // returns Promise[Object] constining response
